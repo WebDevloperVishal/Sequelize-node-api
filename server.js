6f89ccfff91c239cfb5a365044aed1bf0bed0ae0
WebDevloperVisha <vishalsanam2024@gmail.com>
2025-08-29 12:41:39 +0530

    first commit

--- server.js
+++ server.js
@@ -5,69 +5,68 @@ const app = express()
 const port = 3000
 
 sequelize.authenticate()
-.then(() =>
-    console.log('Database * Table Created!'))
-.catch((err)=>
-    console.log(err))
+    .then(() =>
+        console.log('Database * Table Created!'))
+    .catch((err) =>
+        console.log(err))
 
 app.use(express.json())
-app.use(express.urlencoded({extended:true}))
+app.use(express.urlencoded({ extended: true }))
 
 // Read All Contacts
-app.get('/contacts',async (req, res) => {
+app.get('/contacts', async (req, res) => {
     try {
         const contacts = await Contact.findAll()
         res.json(contacts)
     } catch (err) {
-        res.status(500).json({error: err.message})
+        res.status(500).json({ error: err.message })
     }
 })
 
 // Read Single Contacts
-app.get('/contacts/:id',async (req, res) => {
+app.get('/contacts/:id', async (req, res) => {
     try {
         const contact = await Contact.findByPk(req.params.id)
-        if(!contact) return res.status(404).json({ error: "Contact not found"})
+        if (!contact) return res.status(404).json({ error: "Contact not found" })
         res.json(contact)
     } catch (err) {
-        res.status(500).json({error: err.message})
+        res.status(500).json({ error: err.message })
     }
 })
 
 // Create Contacts
-app.post('/contacts',async (req, res) => {
+app.post('/contacts', async (req, res) => {
     try {
         const contact = await Contact.create(req.body)
         res.json(contact)
     } catch (err) {
-        res.status(500).json({error: err.message})
+        res.status(500).json({ error: err.message })
     }
 })
 
 // Update Contacts
-app.put('/contacts/:id',async (req, res) => {
+app.put('/contacts/:id', async (req, res) => {
     try {
         const contact = await Contact.findByPk(req.params.id)
-        if(!contact) return res.status(404).json({ error: "Contact not  found"})
+        if (!contact) return res.status(404).json({ error: "Contact not  found" })
 
-            await Contact.update(req.body, { where : { id: req.params.id}})
-            const updated_contact = await Contact.findByPk(req.params.id)
+        await Contact.update(req.body, { where: { id: req.params.id } })
+        const updated_contact = await Contact.findByPk(req.params.id)
         res.json(updated_contact)
     } catch (err) {
-        res.status(500).json({error: err.message})
+        res.status(500).json({ error: err.message })
     }
 })
 
 // Delete Contacts
-app.delete('/contacts/:id',async (req, res) => {
+app.delete('/contacts/:id', async (req, res) => {
     try {
         const contact = await Contact.findByPk(req.params.id)
-        if(!contact) return res.status(404).json({ error: "Contact not  found"})
-
-            await contact.destroy()
-        res.json({message: "Contact deleted"})
+        if (!contact) return res.status(404).json({ error: "Contact not  found" })
+        await contact.destroy()
+        res.json({ message: "Contact deleted" })
     } catch (err) {
-        res.status(500).json({error: err.message})
+        res.status(500).json({ error: err.message })
     }
 })
 
